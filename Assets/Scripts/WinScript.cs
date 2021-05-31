30d943b96e023d61bb5133d5601505271fff0e7d
Bobby <69677528+RobertW2@users.noreply.github.com>
2021-05-31 13:03:22 +1000

    Update music and background, Not Working

--- Assets/Scripts/WinScript.cs
+++ Assets/Scripts/WinScript.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class WinScript : MonoBehaviour
 {
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         FindObjectOfType<HighScoreSave>().SaveBinary();
 
         SceneManager.LoadScene("WinScene");
 
+
     }
+
 }
